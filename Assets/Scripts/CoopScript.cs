5513c3db50b3758cf5854d836240875f5ad35777
Céline <cceline74@hotmail.fr>
2020-10-01 12:30:50 +0200

    Ajout du policier (Capsule test + cercle)

---
+++ Assets/Scripts/CoopScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoopScript : MonoBehaviour
+{
+    private float Speed;
+    Vector3 direction = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        direction = new Vector3(1, 0, 1).normalized;
+
+        gameObject.transform.position += direction * Time.deltaTime * Speed;
+
+        gameObject.transform.rotation = Quaternion.LookRotation(direction, Vector3.up);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
