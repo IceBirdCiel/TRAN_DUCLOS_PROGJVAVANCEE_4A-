8a2b993128192c91e5f752e5f8b3f44961e7f2b4
Céline <cceline74@hotmail.fr>
2020-10-02 13:40:53 +0200

    script de la lumière pour le main menu

---
+++ Assets/Scripts/LightScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightScript : MonoBehaviour
+{
+    [SerializeField]
+    private Light light;
+
+    private Color color;
+
+    private float time;
+    private int x;
+
+    private void Start()
+    {
+        time = 1;
+        x = 0;
+        light.color = Color.red;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime*4.0f;
+        x = (int)time;
+
+        Debug.Log(x);
+        if((x/2)%2 == 0)
+        {
+            color = Color.red;
+        }
+        else
+        {
+            color = Color.blue;
+        }
+
+        light.color = color;
+    }
+}
